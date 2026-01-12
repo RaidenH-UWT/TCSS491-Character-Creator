af3e7c2bbce7e8f2b211e700072f1a4a09915988
RaidenH-UWT <raidenh@uw.edu>
2026-01-12 10:46:14 -0800

    figuring out filesystem ops in js

--- site/scripts/extension_loader.js
+++ site/scripts/extension_loader.js
@@ -1,5 +1,33 @@
-import { parse, stringify } from 'https://esm.sh/smol-toml@1.6.0'
+import TOML from 'https://esm.sh/smol-toml@1.6.0'
 
-const doc = "key = 'value'";
-const parsed = parse(doc);
-console.log(parsed);
+var demoSpec;
+
+async function loadTOML() {
+    let demoFile = await fetch("../extensions/demo/specification.toml")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response not OK");
+            }
+            return response.text();
+        });
+
+    demoSpec = TOML.parse(demoFile);
+    console.log(demoSpec);
+    console.log(demoSpec.name);
+}
+
+async function listFolders() {
+    let dirListing = await fetch("../extensions").then(response => {
+        if (!response.ok) {
+            throw new Error("Network response not OK");
+        }
+        return response.text();
+    });
+    console.log(dirListing);
+    let dirList = dirListing.getElementsByTag("ul")[0];
+
+    console.log(dirList);
+}
+
+// await loadTOML();
+await listFolders();
