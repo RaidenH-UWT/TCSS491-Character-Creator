408e3de7c4307af5a2dc881d9b834488eaa4835a
RaidenH-UWT <raidenh@uw.edu>
2026-01-12 19:15:03 -0800

    setup TOML importing for specs and redesigned home page

---
+++ site/scripts/asset.js
@@ -0,0 +1,7 @@
+/**
+ * This is a class representing a basic asset
+ * @param config the data representing this asset, which contains all the necessary information
+ */
+class Asset(config) {
+    
+}
--- site/scripts/extension_loader.js
+++ site/scripts/extension_loader.js
@@ -1,33 +1,47 @@
-import TOML from 'https://esm.sh/smol-toml@1.6.0'
+// List of the names of premade extensions included in the program
+const extensions = ["demo", "dragon"];
 
-var demoSpec;
+const extensionSpec = [];
 
-async function loadTOML() {
-    let demoFile = await fetch("../extensions/demo/specification.toml")
+/**
+ * Loads the TOML data from each extension specified in the extensions list
+ */
+async function loadExtensions() {
+    // Load the smol-toml library to parse extensions
+    const TOML = await import("https://cdn.jsdelivr.net/npm/smol-toml@1.6.0/+esm");
+    
+    for (let i = 0; i < extensions.length; i++) {
+        let extText = await fetch(`../extensions/${extensions[i]}/specification.toml`)
         .then(response => {
             if (!response.ok) {
                 throw new Error("Network response not OK");
             }
             return response.text();
         });
+        
+        extensionSpec[i] = TOML.parse(extText);
+        console.log(extensionSpec[i].name);
+    }
 
-    demoSpec = TOML.parse(demoFile);
-    console.log(demoSpec);
-    console.log(demoSpec.name);
+    console.log(extensionSpec);
 }
 
-async function listFolders() {
-    let dirListing = await fetch("../extensions").then(response => {
-        if (!response.ok) {
-            throw new Error("Network response not OK");
-        }
-        return response.text();
-    });
-    console.log(dirListing);
-    let dirList = dirListing.getElementsByTag("ul")[0];
-
-    console.log(dirList);
+/**
+ * Load an extension file (.zip) from the user
+ * @param theFile .zip archive to load
+ */
+async function loadUserExtension(theFile) {
+    const zip = await import("https://cdn.jsdelivr.net/npm/jszip@3.10.1/dist/jszip.min.js");
 }
 
-// await loadTOML();
-await listFolders();
+// async function listFolders() {
+//     let dirListing = await fetch("../extensions").then(response => {
+//         if (!response.ok) {
+//             throw new Error("Network response not OK");
+//         }
+//         return response.text();
+//     });
+//     // i could do some unholy code in here to grab the folder names from the returned
+//     // page, but i'm just gonna hardcode the premade extensions and handle user uploaded
+//     // extensions seperately.
+// }
